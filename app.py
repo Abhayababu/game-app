bfeaac2e8d5415395cdf7bc3506de352351285ed
Abhayababu <abhayaps3@gmail.com>
2025-01-31 10:47:47 +0530

    game changes message

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
-from flask import Flask,request,render_template
+from flask import Flask, request, render_template
 import random
-app=Flask(__name__)
-choices=["stone","paper","scissor"]
-@ app.route("/",methods=["POST"])
+
+app = Flask(__name__)
+
+choices = ["stone", "paper", "scissor"]
+
+@app.route("/", methods=["GET", "POST"])
 def explore():
-    if request.method=="POST":
-        my_choice=request.form.get("choice")
-        computer_choice=random.choice(choices)
-        if my_choice==computer_choice:
-            result="it is tie"
-        elif (my_choice=="stone"and computer_choice=="scissor")or\
-             (my_choice=="paper"and computer_choice=="stone")or\
-             (my_choice=="scissor" and computer_choice=="paper"):
-            result="you won the game"
+    if request.method == "POST":
+        my_choice = request.form.get("choice")
+        computer_choice = random.choice(choices)
+        if my_choice == computer_choice:
+            result = "It is a tie"
+        elif (my_choice == "stone" and computer_choice == "scissor") or \
+             (my_choice == "paper" and computer_choice == "stone") or \
+             (my_choice == "scissor" and computer_choice == "paper"):
+            result = "You won the game"
         else:
-            result="computer won the game"
-        return render_template("home.html",my_choice=my_choice,computer_choice=computer_choice,result=result)
-    return render_template("home.html",my_choice=None,computer_choice=None,result=None)
+            result = "Computer won the game"
+        return render_template("home.html", my_choice=my_choice, computer_choice=computer_choice, result=result)
+    return render_template("home.html", my_choice=None, computer_choice=None, result=None)
+
 if __name__ == "__main__":
     app.run(debug=True)
-    
